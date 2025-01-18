c9b2023f52121e9fe9923e9321ef4742873a1d1b
choiak <choichaklong@gmail.com>
2025-01-19 00:52:51 +0800

    Add greet and exit

--- src/main/java/InuChan.java
+++ src/main/java/InuChan.java
@@ -1,4 +1,49 @@
 public class InuChan {
+    static String inuHappy =
+            """
+                        __    __
+                        \\/----\\/
+                         \\^  ^/    WOOF!
+                         _\\  /_
+                       _|  \\/  |_
+                      | | |  | | |
+                     _| | |  | | |_
+                    "---|_|--|_|---"
+                    
+                    """;
+    static String inuSad =
+            """
+                        __    __
+                        \\/----\\/
+                         \\Q  Q/    AWOO!
+                         _\\  /_
+                       _|  \\/  |_
+                      | | |  | | |
+                     _| | |  | | |_
+                    "---|_|--|_|---"
+                    
+                    """;
+
+    static void greet() {
+        System.out.println(inuHappy + "Hi there! I'm Inu-chan, woof!");
+        ask("How can I help you");
+    }
+
+    static void sayBye() {
+        System.out.println(inuSad + "Woof! I will be waiting you! See you, awooooo!");
+    }
+
+    /**
+     * Print the question being asked with modification to fit Inu-chan's character.
+     *
+     * @param question the actual question being asked (without any ending punctuation like question mark '?').
+     */
+    static void ask(String question) {
+        System.out.println(question + ", arf arf?");
+    }
+
     public static void main(String[] args) {
+        greet();
+        sayBye();
     }
 }
