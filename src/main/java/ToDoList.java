b8bd4c0de80c8760a9458f341e4eab9b0d2b5543
choiak <choichaklong@gmail.com>
2025-01-29 21:39:38 +0800

    Debug ToDoList.java

--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -23,7 +23,7 @@ public class ToDoList {
      * Return -1 if the list is full.
      *
      * @param name The name of the task.
-     * @return task id, -1 if the list is full.
+     * @return Task id, -1 if the list is full.
      */
     public Integer addTask(String name) {
         if (numCurrentItem < capacity) {
@@ -35,17 +35,38 @@ public class ToDoList {
         }
     }
 
-    public void markTaskDone(Integer id) {
-        list[id - 1].markAsDone();
-    }
+    /**
+     * Mark the task of the given id as the required state.
+     * Return the task id.
+     * Return negative value if the id is invalid or the task is already in the required state.
+     *
+     * @param id The id of the task.
+     * @param isDone The required state of the task.
+     * @return Task id, -1 if the id is invalid, -2 if the task is already in the required state.
+     */
+    public Integer markTask(Integer id, Boolean isDone) {
+        if (id < 1 || id > numCurrentItem) {
+            return -1;
+        } else if ((isDone && list[id - 1].getIsDone()) || (!isDone && !list[id - 1].getIsDone())) {
+            return -2;
+        } else {
+            if (isDone) {
+                list[id - 1].markAsDone();
+            } else {
+                list[id - 1].markAsNotDone();
+            }
+            return id;
+        }
 
-    public void markTaskNotDone(Integer id) {
-        list[id - 1].markAsNotDone();
     }
 
     public void printList() {
         for (int i = 0; i < numCurrentItem; i++) {
             list[i].printTask();
         }
+    }
+
+    public void printTask(Integer id) {
+        list[id - 1].printTask();
     }
 }
