e9592085e079a2768ce543051035c0b741cc9fa5
choiak <choichaklong@gmail.com>
2025-02-09 20:48:10 +0800

    Refactor: reimplement tokenize with methods

--- src/main/java/inuChan/InuChan.java
+++ src/main/java/inuChan/InuChan.java
@@ -156,6 +156,79 @@ public class InuChan {
         }
     }
 
+    private static String[] tokenizeMarkUnmark(String command, String firstWord) throws InvalidArgumentCount {
+        String[] spaceSeparatedToken = command.split(" +");
+        int wordCount = spaceSeparatedToken.length;
+        if (wordCount != 2) {
+            throw new InvalidArgumentCount();
+        }
+        return new String[]{firstWord, spaceSeparatedToken[1]};
+    }
+
+    private static String[] tokenizeTodo(String command) throws InvalidArgument {
+        if (command.contains("/by") || command.contains("/from") || command.contains("/to")) {
+            throw new InvalidArgument();
+        }
+        return new String[]{"todo", command.substring("todo".length()).strip()};
+    }
+
+    private static String[] tokenizeDeadline(String command) throws InvalidArgument, InvalidArgumentCount {
+        int argumentCount = command.split("/by").length - 1;
+        if (command.contains("/from") || command.contains("/to")) {
+            throw new InvalidArgument();
+        } else if (argumentCount != 1) {
+            throw new InvalidArgumentCount();
+        } else {
+            String deadlineName = command.split("/by")[0].substring("deadline".length()).strip();
+            String argumentBy = command.split("/by")[1].strip();
+
+            if (deadlineName.isEmpty()) {
+                throw new InvalidArgumentCount();
+            }
+
+            return new String[]{"deadline",
+                    deadlineName,
+                    argumentBy};
+        }
+    }
+
+    private static String[] tokenizeEvent(String command) throws InvalidArgument, InvalidArgumentCount {
+        int argumentCount = command.split("/by|/from|/to").length - 1;
+        if (command.contains("/by")) {
+            throw new InvalidArgument();
+        } else if (argumentCount != 2) {
+            throw new InvalidArgumentCount();
+        } else {
+            String eventName = command.split("/from")[0].substring("event".length()).strip();
+            String argumentFrom = command.split("/from")[1].strip();
+
+            String argumentTo;
+            if (argumentFrom.contains("/to")) {
+                argumentTo = argumentFrom.split("/to")[1].strip();
+                argumentFrom = argumentFrom.split("/to")[0].strip();
+            } else {
+                argumentTo = eventName.split("/to")[1].strip();
+                eventName = eventName.split("/to")[0].strip();
+            }
+
+            if (eventName.isEmpty()) {
+                throw new InvalidArgumentCount();
+            }
+
+            return new String[]{"event",
+                    eventName,
+                    argumentFrom,
+                    argumentTo};
+        }
+    }
+
+    private static String[] tokenizeByeList(String command, String firstWord) throws InvalidArgumentCount {
+        if (command.split(" +").length > 1) {
+            throw new InvalidArgumentCount();
+        }
+        return new String[]{firstWord};
+    }
+
     /**
      * Tokenize the input command.
      * Return an array of tokens.
@@ -163,83 +236,19 @@ public class InuChan {
      *
      * @param command The input command.
      * @return Array of tokens and its respective arguments.
+     * @throws InvalidCommand If there is no such command.
+     * @throws InvalidArgument If there is an invalid argument.
+     * @throws InvalidArgumentCount If there is an invalid number of arguments.
      */
     public static String[] tokenize(String command) throws InvalidCommand, InvalidArgument, InvalidArgumentCount {
-        String[] spaceSeparatedToken = command.split(" +");
-
-        String firstWord = spaceSeparatedToken[0];
-        int wordCount = spaceSeparatedToken.length;
-
-        boolean hasArgumentBy = command.contains("/by");
-        boolean hasArgumentFrom = command.contains("/from");
-        boolean hasArgumentTo = command.contains("/to");
-        int argumentCount = command.split("/by|/from|/to").length - 1;
+        String firstWord = command.split(" +")[0];
 
         return switch (firstWord) {
-            case "mark", "unmark" -> {
-                if (wordCount != 2) {
-                    throw new InvalidArgumentCount();
-                }
-                yield new String[]{firstWord, spaceSeparatedToken[1]};
-            }
-            case "todo" -> {
-                if (hasArgumentBy || hasArgumentFrom || hasArgumentTo) {
-                    throw new InvalidArgument();
-                }
-                yield new String[]{firstWord,
-                        command.substring("todo".length()).strip()};
-            }
-            case "deadline" -> {
-                if (hasArgumentFrom || hasArgumentTo) {
-                    throw new InvalidArgument();
-                } else if (argumentCount != 1) {
-                    throw new InvalidArgumentCount();
-                } else {
-                    String[] arguments = command.split("/by");
-                    String deadlineName = arguments[0].substring("deadline".length()).strip();
-                    String argumentBy = arguments[1].strip();
-                    if (deadlineName.isEmpty()) {
-                        throw new InvalidArgument();
-                    }
-                    yield new String[]{firstWord,
-                            deadlineName,
-                            argumentBy};
-                }
-            }
-            case "event" -> {
-                if (hasArgumentBy) {
-                    throw new InvalidArgument();
-                } else if (argumentCount != 2) {
-                    throw new InvalidArgumentCount();
-                } else {
-                    String eventName = command.split("/from")[0].substring("event".length()).strip();
-                    String argumentFrom = command.split("/from")[1].strip();
-
-                    String argumentTo;
-                    if (argumentFrom.contains("/to")) {
-                        argumentTo = argumentFrom.split("/to")[1].strip();
-                        argumentFrom = argumentFrom.split("/to")[0].strip();
-                    } else {
-                        argumentTo = eventName.split("/to")[1].strip();
-                        eventName = eventName.split("/to")[0].strip();
-                    }
-
-                    if (eventName.isEmpty()) {
-                        throw new InvalidArgument();
-                    }
-
-                    yield new String[]{firstWord,
-                            eventName,
-                            argumentFrom,
-                            argumentTo};
-                }
-            }
-            case "bye", "list" -> {
-                if (wordCount > 1) {
-                    throw new InvalidArgumentCount();
-                }
-                yield new String[]{firstWord};
-            }
+            case "mark", "unmark" -> tokenizeMarkUnmark(command, firstWord);
+            case "todo" -> tokenizeTodo(command);
+            case "deadline" -> tokenizeDeadline(command);
+            case "event" -> tokenizeEvent(command);
+            case "bye", "list" -> tokenizeByeList(command, firstWord);
             default -> throw new InvalidCommand();
         };
     }
